5249791b47cc722f996de98a101f29f9ba191dcc
hassan1717f17 <ihtisham1717f17@gmail.com>
2023-11-13 17:44:35 +0500

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
-
+// server.js
 const express = require('express');
-const app = express();
-const port = 3001;
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const fs = require('fs');
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const app = express();
+const PORT = 3001;
+
+app.use(bodyParser.json());
+
+app.post('/save-to-dropbox', async (req, res) => {
+  const { text } = req.body;
+
+  // Create a text file
+  const fileName = 'textFile.txt';
+  fs.writeFileSync(fileName, text);
+
+  // Use Dropbox Saver to save the file
+  const saverOptions = {
+    files: [
+      {
+        url: `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`,
+        filename: 'textFile.txt',
+      },
+    ],
+  };
+
+  res.status(200).json({ message: 'File saved to Dropbox successfully!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
